4dbc5524f7b3140dab8b63ffc4a506680686f6d9
moeyashi <yorsr315@yahoo.co.jp>
2020-03-29 16:16:19 +0900

    fix lint

--- src/app/lib/firebaseClient.tsx
+++ src/app/lib/firebaseClient.tsx
@@ -7,14 +7,14 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
 const clientCredentials = {
-  apiKey: "AIzaSyDlHyhD4WZZOHsV_mA9DlBLJl9v7FHq5QM",
-  authDomain: "mahjong-7374d.firebaseapp.com",
-  databaseURL: "https://mahjong-7374d.firebaseio.com",
-  projectId: "mahjong-7374d",
-  storageBucket: "mahjong-7374d.appspot.com",
-  messagingSenderId: "367309424099",
-  appId: "1:367309424099:web:a914b87378838a48720cd6",
-  measurementId: "G-VD4ZDKJLMQ"
+  apiKey: 'AIzaSyDlHyhD4WZZOHsV_mA9DlBLJl9v7FHq5QM',
+  authDomain: 'mahjong-7374d.firebaseapp.com',
+  databaseURL: 'https://mahjong-7374d.firebaseio.com',
+  projectId: 'mahjong-7374d',
+  storageBucket: 'mahjong-7374d.appspot.com',
+  messagingSenderId: '367309424099',
+  appId: '1:367309424099:web:a914b87378838a48720cd6',
+  measurementId: 'G-VD4ZDKJLMQ',
 };
 
 // Check that `window` is in scope for the analytics module!
