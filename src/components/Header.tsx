4c5d4be746f3943296a27dd1c15e1dd1696e5f04
moeyashi <yorsr315@yahoo.co.jp>
2020-05-31 17:44:04 +0900

    fix typo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { AppBar, Toolbar } from "@material-ui/core";
 import { useRecoilValue } from "recoil";
 import { uidState } from "hooks/states/userState";
-import { GroupSelectMenu } from "./group/groupSelectMenu";
+import { GroupSelectMenu } from "./group/GroupSelectMenu";
 
 const Header: FC = () => {
   const uid = useRecoilValue(uidState);
